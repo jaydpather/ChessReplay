89f366ec28771ebc8146fbe9996149c3c9ac31b3
Jayd <jaydpather@gmail.com>
2020-01-30 13:06:42 +0100

    able to move 1 piece

--- src/App.fs
+++ src/App.fs
@@ -5,11 +5,23 @@ open Fable.Core.JsInterop
 open Fable.Import
 open Fable.Core.Util
 
+open Browser.Types
+
 [<Emit("alert('$0')")>]
 let alert msg = jsNative
 
-let window = Browser.Dom.window
+let _window = Browser.Dom.window
 
-//alert "hello from chess replay"
+let btnMove_Click e = 
+    let sourceCell:HTMLDivElement = unbox _window.document.getElementById "A2"
+    let destCell:HTMLDivElement = unbox _window.document.getElementById "A3"
+
+    let innerHtml= sourceCell.innerHTML
+    sourceCell.innerHTML <- ""
+    destCell.innerHTML <- innerHtml
+
+let btnMove:Browser.Types.HTMLButtonElement = unbox _window.document.getElementById "btnMove"
+
+btnMove.onclick <- btnMove_Click
 
 printfn "page loaded"
